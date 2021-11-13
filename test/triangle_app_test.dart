2f217c3818df073f1cdc1e3d56e59954f8433bf0
thejo06 <yoshuapurba06@gmail.com>
2021-11-13 17:51:33 +0700

    initial commit

---
+++ test/triangle_app_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:triangle_app/triangle_app.dart';
+
+void main() {
+  group('Detect the triangle', () {
+    /* Test jika salah satu sisa nilai nya < 1 */
+    test('Should throw error when is side less than 1', () {
+      expect(() => detectTriangle(-1, 2, 2), throwsA(isA<Exception>()));
+      expect(() => detectTriangle(1, -2, 2), throwsA(isA<Exception>()));
+      expect(() => detectTriangle(1, 2, -2), throwsA(isA<Exception>()));
+    });
+
+    /* Test, bila sisi a + b bernilai <= c, maka gagal */
+    test('Should throw error when side a + b <= c', () {
+      expect(() => detectTriangle(4, 1, 2), throwsA(isA<Exception>()));
+      expect(() => detectTriangle(5, 1, 3), throwsA(isA<Exception>()));
+    });
+
+    /* Test jika segitiga sama sisi */
+    test('Should return "Segitiga Sama Sisi" when all sides are equal', () {
+      expect(detectTriangle(1, 1, 1), "Segitiga Sama Sisi");
+      expect(detectTriangle(2, 2, 1), isNot("Segitiga Sama Sisi"));
+    });
+
+    /* Test jika segitiga sama kaki */
+    test('Should return "Segitiga Sama Kaki" when only two sides are equal',
+        () {
+      expect(detectTriangle(2, 2, 3), 'Segitiga Sama Kaki');
+      expect(detectTriangle(4, 2, 4), 'Segitiga Sama Kaki');
+      expect(detectTriangle(1, 2, 2), 'Segitiga Sama Kaki');
+      expect(detectTriangle(4, 1, 4), "Segitiga Sama Kaki");
+    });
+
+    test('Should return "Segitiga Sembarang" when no sides are equal', () {
+      expect(detectTriangle(2, 4, 3), "Segitiga Sembarang");
+    });
+  });
+}
